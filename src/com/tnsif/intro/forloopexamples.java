8f2094d3906f6755b8e63677ba59a0cdd3f58908
akshi <akshi@LAPTOP-MRFC31HF>
2026-01-25 20:44:37 +0530

    forloop

---
+++ src/com/tnsif/intro/forloopexamples.java
@@ -0,0 +1,11 @@
+package com.tnsif.intro;
+
+public class forloopexamples {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			System.out.println(i);
+			}
+
+
+			}
+			}
